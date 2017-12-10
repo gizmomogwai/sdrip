1eee19e50036a210df17fe23f637da532accbd99
Christian Koestlin <christian.koestlin@gmail.com>
2017-12-10 12:11:37 +0100

    Update to new raspi installation of ldc

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xe
 sudo swapon /swap || true
-PATH=/home/osmc/bin/ldc2-1.5.0-linux-armhf/bin:$PATH exec dub run -- tcpreceiver
+PATH=/home/osmc/bin/ldc/bin:$PATH exec dub run -- tcpreceiver
